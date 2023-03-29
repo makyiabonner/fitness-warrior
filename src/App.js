ddd511ae4261aebc7ffd5ee9dcb9950f0ef188c2
makyia bonner <makyiabonner@gmail.com>
2023-03-29 17:48:09 -0400

    Navbar and hero section complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <div>
-      <img src='guy-flexing.jpg'></img>
-    </div>
+    <>
+      <div className='hero-section'>
+        <Header />
+        <div className='landing'>
+          <section>
+            <h4>WELCOME TO THE BEST</h4>
+            <h1>WORLD CLASS TRAINING</h1>
+            <a>JOIN TODAY</a>  
+          </section>        
+        </div>
+      </div>
+      <div className='text-line'>
+        <ul>          
+          <li>NEW VIDEOS DAILY</li>
+          <li>BEGINNERS FRIENDLY</li>
+          <li>LIVE CONSULTANCY</li>
+          <li>20+ TRAINERS</li>
+          <li>TRAIN ANYTIME</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
