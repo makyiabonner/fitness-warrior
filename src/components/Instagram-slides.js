980bf651409f48791f88ff9ef721b87551a7d712
makyia bonner <makyiabonner@gmail.com>
2023-04-20 19:23:40 -0400

    (feat): hover effects for footer pics functional

--- src/components/Instagram-slides.js
+++ src/components/Instagram-slides.js
@@ -1,41 +1,64 @@
 import './Instagram-slides.css'
 import React from "react";
-import ReactDOM from "react-dom/client";
 import hoverLogo from '../images/instagram-coloredlogo.svg'
+import pic1 from '..//images/sven-mieke-1MrMsNBcsYA-unsplash.webp'
+import pic2
+import pic3
+import pic4
+import pic5
+import pic6
 
 
 export default function InstaPics(){
+        const pics = [
+            {
+                img:'',
+                alt:''
+            },
+            {
+                img:'',
+                alt:''
+            },
+            {
+                img:'',
+                alt:''
+            },
+            {
+                img:'',
+                alt:''
+            },
+            {
+                img:'',
+                alt:''
+            },
+            {
+                img:'',
+                alt:''
+            },
+        ]
     function MouseOver() {
-        let hover = document.querySelector('.Hover-coat')
+
       }
-      function MouseOut(event){
-        event.target.style.background="";
-        const instap = document.querySelector('.instapics')
-        const root = ReactDOM.createRoot(instap)
-        return(
-        root.render(<></>)
-        )
+      function MouseOut(){
       }
       return (
         <>
         <div className='insta'>
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                <div className='Hover-coat'>
+            {pics.map(pic => {
+                return (
+                <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}
+                    style={{
+                        background:`url(${pic.img})`
+                    }}>
+                    <div className='Hover-coat'>
                     <a href='' alt='Instagram Logo'>
                     <img src={hoverLogo} className='Hover-logo'></img>
-                </a>
-            </div>
-                </div>        
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                 </div>
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                 </div>
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                </div>
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                </div>
-            <div className='instapics' onMouseOver={MouseOver} onMouseOut={MouseOut}>
-                </div>        
+                    </a>
+                    </div>
+                </div>   
+                )
+            })}
+              
         </div>
         </>
     )
